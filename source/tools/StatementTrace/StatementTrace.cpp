832b688f65ea6729e98b6e4801fe4db26bff02cc
bstee615 <benjaminjsteenhoek@gmail.com>
2020-10-30 23:15:43 -0500

    Print trace when program is run

--- source/tools/StatementTrace/StatementTrace.cpp
+++ source/tools/StatementTrace/StatementTrace.cpp
@@ -11,25 +11,13 @@ using std::cerr;
 using std::string;
 using std::endl;
 
-/* ================================================================== */
-// Global variables 
-/* ================================================================== */
-
-UINT64 insCount = 0;        //number of dynamically executed instructions
-UINT64 bblCount = 0;        //number of dynamically executed basic blocks
-UINT64 threadCount = 0;     //total number of threads, including main thread
-
 std::ostream * out = &cerr;
 
 /* ===================================================================== */
 // Command line switches
 /* ===================================================================== */
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE,  "pintool",
-    "o", "", "specify file name for MyPinTool output");
-
-KNOB<BOOL>   KnobCount(KNOB_MODE_WRITEONCE,  "pintool",
-    "count", "1", "count instructions, basic blocks and threads in the application");
-
+    "o", "", "specify file name for StatementTrace output");
 
 /* ===================================================================== */
 // Utilities
@@ -40,65 +28,36 @@ KNOB<BOOL>   KnobCount(KNOB_MODE_WRITEONCE,  "pintool",
  */
 INT32 Usage()
 {
-    cerr << "This tool prints out the number of dynamically executed " << endl <<
-            "instructions, basic blocks and threads in the application." << endl << endl;
+    cerr << "This tool prints out a dynamic trace at every statement in the application ";
 
     cerr << KNOB_BASE::StringKnobSummary() << endl;
 
     return -1;
 }
 
-/* ===================================================================== */
-// Analysis routines
-/* ===================================================================== */
+struct Location {
+    INT32 column;
+    INT32 line;
+    string filename;
+};
+std::vector<Location> locations;
 
 /*!
- * Increase counter of the executed basic blocks and instructions.
- * This function is called for every basic block when it is about to be executed.
- * @param[in]   numInstInBbl    number of instructions in the basic block
- * @note use atomic operations for multi-threaded applications
- */
-VOID CountBbl(UINT32 numInstInBbl)
-{
-    bblCount++;
-    insCount += numInstInBbl;
-}
-
-/* ===================================================================== */
-// Instrumentation callbacks
-/* ===================================================================== */
-
-/*!
- * Insert call to the CountBbl() analysis routine before every basic block 
- * of the trace.
+ * Log every step of the trace.
+ * 
  * This function is called every time a new trace is encountered.
- * @param[in]   trace    trace to be instrumented
+ * @param[in]   ins      instruction to be instrumented
  * @param[in]   v        value specified by the tool in the TRACE_AddInstrumentFunction
  *                       function call
  */
-VOID Trace(TRACE trace, VOID *v)
+VOID OnInstruction(INS ins, VOID *v)
 {
-    // Visit every basic block in the trace
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
-    {
-        // Insert a call to CountBbl() before every basic bloc, passing the number of instructions
-        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)CountBbl, IARG_UINT32, BBL_NumIns(bbl), IARG_END);
+    Location l;
+    LEVEL_PINCLIENT::PIN_GetSourceLocation(INS_Address(ins), &l.column, &l.line, &l.filename);
+    // *out << l.filename << ":" << l.line << endl;
+    if (!l.filename.empty()) {
+        locations.push_back(l);
     }
-}
-
-/*!
- * Increase counter of threads in the application.
- * This function is called for every thread created by the application when it is
- * about to start running (including the root thread).
- * @param[in]   threadIndex     ID assigned by PIN to the new thread
- * @param[in]   ctxt            initial register state for the new thread
- * @param[in]   flags           thread creation flags (OS specific)
- * @param[in]   v               value specified by the tool in the 
- *                              PIN_AddThreadStartFunction function call
- */
-VOID ThreadStart(THREADID threadIndex, CONTEXT *ctxt, INT32 flags, VOID *v)
-{
-    threadCount++;
 }
 
 /*!
@@ -111,10 +70,10 @@ VOID ThreadStart(THREADID threadIndex, CONTEXT *ctxt, INT32 flags, VOID *v)
 VOID Fini(INT32 code, VOID *v)
 {
     *out <<  "===============================================" << endl;
-    *out <<  "MyPinTool analysis results: " << endl;
-    *out <<  "Number of instructions: " << insCount  << endl;
-    *out <<  "Number of basic blocks: " << bblCount  << endl;
-    *out <<  "Number of threads: " << threadCount  << endl;
+    *out <<  "StatementTrace analysis results: " << endl;
+    for (auto i = locations.begin(); i != locations.end(); i ++) {
+        *out << "Location: " << (*i).filename << ":" << (*i).line << endl;
+    }
     *out <<  "===============================================" << endl;
 }
 
@@ -134,24 +93,20 @@ int main(int argc, char *argv[])
         return Usage();
     }
     
+    PIN_InitSymbols();
+    
     string fileName = KnobOutputFile.Value();
 
     if (!fileName.empty()) { out = new std::ofstream(fileName.c_str());}
 
-    if (KnobCount)
-    {
-        // Register function to be called to instrument traces
-        TRACE_AddInstrumentFunction(Trace, 0);
+    // Register function to be called to instrument traces
+    INS_AddInstrumentFunction(OnInstruction, 0);
 
-        // Register function to be called for every thread before it starts running
-        PIN_AddThreadStartFunction(ThreadStart, 0);
-
-        // Register function to be called when the application exits
-        PIN_AddFiniFunction(Fini, 0);
-    }
+    // Register function to be called when the application exits
+    PIN_AddFiniFunction(Fini, 0);
     
     cerr <<  "===============================================" << endl;
-    cerr <<  "This application is instrumented by MyPinTool" << endl;
+    cerr <<  "This application is instrumented by StatementTrace" << endl;
     if (!KnobOutputFile.Value().empty()) 
     {
         cerr << "See file " << KnobOutputFile.Value() << " for analysis results" << endl;
