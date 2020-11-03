78b79934e395c448f3e37179e45c7d03f15cbf5e
bstee615 <benjaminjsteenhoek@gmail.com>
2020-11-03 00:17:53 -0600

    Changes to .giitgnore and test.c

--- source/tools/StatementTrace/test.c
+++ source/tools/StatementTrace/test.c
@@ -1,6 +1,15 @@
+int v;
+
+int sum(int x, int y) {
+    return x + y;
+}
+
 int main()
 {
-    int a = 0;
+    int a = v;
+    {
+        int nested = 5;
+    }
     int b = 1;
     for (int i = 0; i < 5; i ++) {
         a = i;
