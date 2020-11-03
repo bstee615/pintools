f5cb00a355d5e541e8cfff6eeba3d57021a6faa1
bstee615 <benjaminjsteenhoek@gmail.com>
2020-11-02 23:09:42 -0600

    Filter to only in-scoped local variables

--- source/tools/StatementTrace/go/fault-location.go
+++ source/tools/StatementTrace/go/fault-location.go
@@ -21,7 +21,13 @@ func main() {
 		},
 	}
 	f := faults(m, faultLocations)
-	fmt.Printf("%v\n", f)
+	for loc, vs := range f {
+		fmt.Printf("%v: [ ", loc)
+		for _, v := range vs {
+			fmt.Printf("%s ", v.Name)
+		}
+		fmt.Printf("]\n")
+	}
 }
 
 type FaultLocation struct {
@@ -29,17 +35,12 @@ type FaultLocation struct {
 	filename   string
 }
 
-type Decl struct {
-	lineNumber int64
-	name       string
-}
-
-func faults(mod *ir.Module, faultLocations []FaultLocation) map[FaultLocation][]Decl {
+func faults(mod *ir.Module, faultLocations []FaultLocation) map[FaultLocation][]*metadata.DILocalVariable {
 	fmt.Printf("SourceFilename %s\n", mod.SourceFilename)
-	funcs := make(map[*ir.Func][]FaultLocation, 0)
+	locationsByFunction := make(map[*ir.Func][]FaultLocation, 0)
 
 	for _, f := range mod.Funcs {
-		funcs[f] = make([]FaultLocation, 0)
+		locationsByFunction[f] = make([]FaultLocation, 0)
 		var begin int64
 		if f.Metadata != nil {
 			begin = f.Metadata[0].Node.(*metadata.DISubprogram).Line
@@ -50,47 +51,59 @@ func faults(mod *ir.Module, faultLocations []FaultLocation) map[FaultLocation][]
 				var end int64 = t.Metadata[0].Node.(*metadata.DILocation).Line // TODO
 				fmt.Printf("%s\n", t)
 				for _, loc := range faultLocations {
-					if begin < loc.lineNumber && loc.lineNumber < end {
-						funcs[f] = append(funcs[f], loc)
+					if loc.filename == mod.SourceFilename && begin < loc.lineNumber && loc.lineNumber < end {
+						locationsByFunction[f] = append(locationsByFunction[f], loc)
 					}
 				}
 			}
 		}
 	}
+	fmt.Printf("%v\n", locationsByFunction)
 
-	fmt.Printf("%v\n", funcs)
-
-	ret := make(map[FaultLocation][]Decl, 0)
-
-	// For each function of the module.
+	ret := make(map[FaultLocation][]*metadata.DILocalVariable, 0)
 	for _, f := range mod.Funcs {
-		if f.Metadata != nil {
-			fmt.Printf("func %s\n", f.Metadata[0].Node.(*metadata.DISubprogram).Name)
-		}
+		vars := make([]*metadata.DILocalVariable, 0)
 		for _, block := range f.Blocks {
 			for _, inst := range block.Insts {
-				if call_inst, ok := inst.(*ir.InstCall); ok {
-					if call_inst.Callee.(*ir.Func).GlobalIdent.GlobalName == "llvm.dbg.declare" {
-						a := call_inst.Args[1].(*metadata.Value).Value.(*metadata.DILocalVariable)
-						fmt.Printf("var %s\n", a.Name)
+				switch inst := inst.(type) {
+				case *ir.InstCall:
+					// LLVM calls a dbg function whenever a local variable or parameter is declared
+					if inst.Callee.(*ir.Func).GlobalIdent.GlobalName == "llvm.dbg.declare" {
+						a := inst.Args[1].(*metadata.Value).Value.(*metadata.DILocalVariable)
+						vars = append(vars, a)
+					}
+				case *ir.InstAdd:
+					for _, loc := range locationsByFunction[f] {
+						inst_loc, ok := inst.Metadata[0].Node.(*metadata.DILocation)
+						if ok && inst_loc.Line == loc.lineNumber {
+							// This is the line of our fault location. Get all variables in scope
+							in_scope := make([]*metadata.DILocalVariable, 0)
+							inst_scope := inst_loc.Scope
+							for _, v := range vars {
+								p := inst_scope
+								at_root := false
+								for !at_root {
+									if p == v.Scope {
+										in_scope = append(in_scope, v)
+									}
 
-						// Mark all the variables declared before this fault location
-						for _, loc := range funcs[f] {
-							if mod.SourceFilename == loc.filename && a.Line < loc.lineNumber {
-								ret[loc] = append(ret[loc], Decl{
-									lineNumber: a.Line,
-									name:       a.Name,
-								})
+									switch pt := p.(type) {
+									case *metadata.DILocation:
+										p = pt.Scope
+									case *metadata.DILexicalBlock:
+										p = pt.Scope
+									case *metadata.DISubprogram:
+										p = pt.Scope
+									case *metadata.DIFile:
+										// Done
+										at_root = true
+									}
+								}
 							}
+							ret[loc] = in_scope
 						}
 					}
 				}
-			}
-			// Terminator of basic block.
-			switch term := block.Term.(type) {
-			case *ir.TermRet:
-				// do something.
-				_ = term
 			}
 		}
 	}
