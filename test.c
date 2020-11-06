5d59e0e27e9d0a1fc779825496a3a440af0be1b5
bstee615 <benjaminjsteenhoek@gmail.com>
2020-11-06 16:59:24 -0600

    Add some complexity to the test program

--- test.c
+++ test.c
@@ -1,5 +1,12 @@
 int v;
 
+int multiline(int a) {
+    a --;
+    a --;
+    a ++;
+    return a;
+}
+
 int sum(int x, int y) {
     return x + y;
 }
@@ -12,8 +19,13 @@ int main()
     }
     int b = 1;
     for (int i = 0; i < 5; i ++) {
-        a = i;
-        b = i+1;
-        int c = a + b;
+        a = sum(i,b);
+        b = multiline(a);
     }
+    for (int j = 0; j < 3; j ++) {
+        int c = a + b;
+        b = a - j + c;
+    }
+
+    return 0;
 }
